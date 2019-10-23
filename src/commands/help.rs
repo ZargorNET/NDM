563b62cf6fc860bd93c750b25d4c75f82c4840b2
ZargorNET <zargor3@gmail.com>
2019-10-23 21:46:55 +0200

    Implemented a help better command

--- src/commands/help.rs
+++ src/commands/help.rs
@@ -1,20 +1,33 @@
+use serenity::utils::Colour;
+
 use crate::command_framework::{Command, CommandArguments, CommandResult};
 
 pub static HELP_COMMAND: Command = Command {
     key: "help",
-    description: "hey",
+    description: "",
     help_page: "",
-    category: "Misc",
+    category: "",
     func: help_command,
 };
 
 fn help_command(args: CommandArguments) -> CommandResult {
     let handler = args.handler.read();
-    let cmds = handler.get_all_commands();
-    let mut s = String::new();
-    for command in cmds.iter() {
-        s.push_str(&format!("{} - {} - ``{}``\n", command.key, command.description, command.help_page));
-    }
-    let _ = args.m.reply(args.ctx, s);
+    let mut cmds = handler.get_all_commands().clone();
+    cmds.sort_by(|a, b| a.category.cmp(b.category));
+    let _ = args.m.channel_id.send_message(&args.ctx, |mb| {
+        mb.embed(|mut eb| {
+            eb.title("Help");
+            let mut s = String::new();
+            for cmd in cmds.into_iter() {
+                if cmd.key == "help" { continue; }
+                s.push_str(&format!("``{}`` => {}\n", cmd.help_page, cmd.description));
+            }
+            eb.description(s);
+            eb.color(Colour::from_rgb(39, 174, 96));
+            super::util::add_footer(&mut eb, &args);
+            super::util::add_timestamp(&mut eb);
+            eb
+        })
+    });
     Ok(true)
 }
